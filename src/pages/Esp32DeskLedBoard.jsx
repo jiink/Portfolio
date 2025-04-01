ec14037edba6536c717f0823a2cd7060e5a195ef
Jacob C <jacob.j.carlson03@gmail.com>
2025-04-01 08:19:10 -0700

    ditto

--- src/pages/Esp32DeskLedBoard.jsx
+++ src/pages/Esp32DeskLedBoard.jsx
@@ -14,7 +14,7 @@ function Esp32DeskLedBoard() {
       <p>it's a HUB75 panel</p>
       - esp32 
 
-      the process
+      the journey
       - first, with a hub75 panel, I tried driving it with an arduino mega 2560
       - putting graphics on the panel worked fine
       - one goal was to make this not look like some makeshift, delicate device with exposed wires and pieces hanging around
@@ -60,8 +60,55 @@ function Esp32DeskLedBoard() {
         - I soldered headers onto a prototyping board and it fit. then I soldered more headers, and connections between the wires, which was miserably tedious and difficult. 
         - but this made it so I simply plug my esp32 dev board into my prototype board, then plug my prototype board into the hub75 panel. and it worked! And this looked so much neater than having all those wires everywhere
         - at this time I also wanted to try designing a custom PCB. Since my prototype worked, I learned a bit of kicad, made the schematic, designed the PCB, and got it shipped.
+        - making a schematic is very useful, and I think I should make these more often even when a PCB is not involved.
+        - making the PCB from the schematic was easier than I thought. However I was expecting that the computer would be able to draw all the traces for me; I was surprised I had to draw each one manually. In KiCad I had to draw a path between every pair of pads it showed me, and the paths could not cross unless they were on separate layers, where they showed as separate colors. 
+          - I thought this was eerily similar to certain puzzle games you'd find on smartphones. 
+          - Eventually, the puzzle became too hard for me and seemed impossible. That's when I learned about vias, which lets any path continue on another layer. This made things a lot easier.
         - after getting the PCB, I soldered the pin headers, fit everything together, and it was great. It was nice to not have a lot of delicate wires hanging around.
-      - 
+          - If i cared for compactness, I would have designed a PCB with the ESP32 soldered on to it, rather than having plus for the devboard to be inserted into. However, that would have taken a lot more time, money, and effort just to save a centimeter or two of height. 
+          - I thought the silkscreen was cool. It's nice to write labels, like "Knob 0" and "Knob 1" over their respective pads
+          - I also liked that I was able to order my PCB in different colors, I thought it would only be green. I picked red!
+        - At this point the LED board worked great, but I wasn't satisfied with it. It didn't look like something that would look good on a nightstand or a desk - the back was exposed, there was both a USB cable and a power cable coming out, and the knobs were just hanging around freely.
+        - Earlier I had 3d printed a diffuser so that the harsh, bright pixels could form squares that make for a more coherent image. A flat piece of material would have blurred all the pixels together, but by including a grid on a 1-layer thick sheet of white filament, the diffusing effect works while keeping the shape of the image. It makes a huge difference visually, and when I see other LED board projects online I think they could benefit from the same thing. If you put a picture of something, a sprite with any detail, it's really hard to tell what it is. But with the diffuser, it's not a problem. It makes as huge difference.
+        - But I needed a back cover, and something to hold the knobs and hide their wires. 
+        - I thought this would be easy to design, as it could just be a box. But I didn't want my LED board to sit at a right angle, I wanted it tilted up a bit since the viewer's head will usually be above it. 
+        - I sketched some ideas and started modelling the housing in the CAD software OnShape. This would be the biggest 3D printed design I've done. 
+        - one thing I had in mind was that it should not look boring. I wanted to take advantage of a 3D printer's capabilities. Rather than printing boxes and plates, I wanted to make it cool.
+        - My first design was, well, a box that holds the LED board, and has holes in it for plugs and a switch. It also had tabs with holes in it so a diffuser could be screwed on to it.
+          - There were also square holes for the legs to be inserted into to hold the board up. The square holes were tilted at an angle so that the whole thing would be tilted up when sitting on its legs
+          - The legs would house the knobs. The knobs' wires would go through the leg and into the hole that attaches them to the board.
+        - This was difficult for me and took a long time. Making a 3d printed toy is one thing, but making a design that fits with electronics and other 3D printed parts is not easy.
+        - For keeping the LED board in the box, I made these weird little pieces that would extend the magnetic bolts that the HUB75 panel comes with so that it would sit in the box.
+        - After 3D printing this, it all worked. It took a long time to assemble, taking at least 40 minutes to screw and wire it together. Multiple times I realized I didn't have the assembly process in mind when designing this, as there were lots of tight spaces. I also tried welded halves of parts together using my soldering iron, and it worked well -- I thought that was better than glue.
+          - Just be careful not to weld something that's already has superglue on it. I heard that if you burn superglue, it makes toxic fumes.
+          - I also learned that I could power both the HUB75 panel and the ESP32 with the same wire. The ESP32 can be powered through one of its pins rather than being plugged in through USB.
+        - One huge frustration with my design was that it was a box with wires between the lid and the body. Once everything's in place, you can't see it. When you open it up, wires are stretched across.
+        - But it did work. I think I lost any photos I had of it. It was cool to finish a 3D design of that size, and it did perform the goal of holding the board at an angle and hiding all exposed electronics. 
+          - However, it was ugly! I think I lost any photos I had of it. It was kind of dissapointing. Given my frustration with the wiring and how hard it was to check on it, I didn't feel safe keeping this powered on my nightstand. 
+          - Also, the USB cable had to hang out, or else I couldn't program it. I heard it's also bad to power the ESP32 through its pin and USB plug simulatenously.
+        - As soon as I had to open it up to adjust something, I gave up on that housing, as I didn't want to spend another hour or two just to open it up and close it again.
+        - I decided to do a redesign. It would require the following:
+          - Less parts
+          - Let the user access the wiring on the back of the HUB75 panel easily
+          - Be prettier
+        - It took a long time to think of a better design. My brother helped with some ideas. 
+        - Eventually I started a new model.
+        - Instead of the HUB75 housing floating off the ground, held up by two legs, it would look like this: [picture]
+        - There was one essential new tool I had that made this design possible: heat set inserts! Because of that, I wouldn't need to access both sides of a hole for the nut and the bolt. Instead I could just screw a bolt in and out from one side. 
+          - this let me make the assembly so much easier. I know that trapped nuts are a thing, but I couldn't do that much on my previous design because of the angles certain parts were printed at. Also, supergluing nuts into place is frustrating.
+        - Printing this was a bit nerve wracking because less parts means each part is bigger, and a failure means another re-print. 
+        - But it worked. I put all the heat set inserts in with a soldering iron, and screwed everything together. 
+          - This was so much better than the previous design, it took only a few minutes to assemble, like a consumer product.
+        - I was able to solve the USB programming cable dilemma, too. I was able to set it up so my ESP32 could be programmed over WiFi! I thought that was really cool and made it a lot more convinient. But if I did need a USB cable, it was no problem to open up the back and plug one in (Well, I did have to maul a cable and crease it at an angle to fit it in there)
+        - I made the back panels clear PETG so you could see inside. It wasn't as transparent as I was hoping, but it's cool to see a couple wires and an LED shine through.
+        - So there it was, my cool LED board in a good-looking housing. Now I could finally clear the workbench and move on.
+        - There was just one problem: the software wasn't good. 
+          - This had become more of a mechanical project than anything; the software had been pretty neglected.
+          - I added a simple clock applet so it could serve a purpose on the nightstand - the particle life would be too distracting at night.
+          - When I look at other LED board projects online, I think the visuals are usually kind of lame. I wanted mine to be impressive looking and have tasteful graphics and motion.
+
+      the software
+      
     </div>
   );
 }
